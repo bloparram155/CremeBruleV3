16dce3a3eae18fd2173758ec7ca974b5b141b5b7
mauricio leon <mauricilion@hotmail.com>
2017-11-26 13:22:10 -0700

    layout ordenado de nuevo

---
+++ CremeBrulev3/CremeBrulev3/Controllers/IndexAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CremeBrulev3.Controllers
+{
+    public class IndexAdminController : Controller
+    {
+        // GET: IndexAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
